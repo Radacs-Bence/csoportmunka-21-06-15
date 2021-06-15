6b6a97610ee63f2fd96c7bc70c265eeccd8afb8c
BirkasTivadar <birkastivadar@gmail.com>
2021-06-15 21:54:17 +0200

    Dinamikus teszt kész

--- src/test/java/games/GameDynamicTest.java
+++ src/test/java/games/GameDynamicTest.java
@@ -1,14 +1,37 @@
 package games;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameDynamicTest {
 
+    Object[][] values = {
+            {0, "Spain"},
+            {5, "Italy"},
+            {1, "Russia"}
+    };
+
+    GameService gameService;
+
+    @BeforeEach
+    void init() {
+        GameRepository gameRepository = new GameRepository();
+        gameRepository.addGameFromCsv("src/main/resources/results.csv");
+        gameService = new GameService(gameRepository);
+    }
+
     @TestFactory
     Stream<DynamicTest> testAllGoals() {
-        return null;
+        return Arrays.stream(values)
+                .map(pair -> DynamicTest.dynamicTest("Test all goals" +
+                                pair[0],
+                        () -> assertEquals(pair[0], gameService.allGoalsOfCountry((String) pair[1]))
+                ));
     }
 }
