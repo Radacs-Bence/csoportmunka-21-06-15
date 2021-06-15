b8bb92024a6de84dbde05135ea601d7cb1de7ac2
BirkasTivadar <birkastivadar@gmail.com>
2021-06-15 21:09:23 +0200

    Paraméterezett teszt kész

---
+++ src/test/java/games/GameDynamicTest.java
@@ -0,0 +1,14 @@
+package games;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.stream.Stream;
+
+public class GameDynamicTest {
+
+    @TestFactory
+    Stream<DynamicTest> testAllGoals() {
+        return null;
+    }
+}
--- src/test/java/games/GameServiceTest.java
+++ src/test/java/games/GameServiceTest.java
@@ -2,6 +2,12 @@ package games;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,24 +15,50 @@ class GameServiceTest {
 
     GameService gameService;
 
+
     @BeforeEach
     void init() {
         GameRepository gameRepository = new GameRepository();
         gameRepository.addGameFromCsv("src/main/resources/results.csv");
+        gameRepository.addGame(new Game("Antigua", "Barbados", 4, 0));
+        gameRepository.addGame(new Game("Belgium", "Barbados", 4, 3));
         gameService = new GameService(gameRepository);
     }
 
     @Test
     void largestGoalDiff() {
+        Game game = gameService.largestGoalDiff();
+        assertEquals("Antigua", game.getFirstCountry());
     }
 
     @Test
-    void allGoalsOfCountryTest() {
+    void allGoalsOfCountryTest(String country) {
         int result = gameService.allGoalsOfCountry("Italy");
         assertEquals(5, result);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Italy", "Wales", "Poland"})
+    void allGoalsOfCountryParametrizedTestWithValueSource(String country) {
+        assertTrue(gameService.allGoalsOfCountry(country) > 0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("createAllGoals")
+    void allGoalsOfCountryParametrizedTestWithMethodSource(int goals, String country) {
+        assertEquals(goals, gameService.allGoalsOfCountry(country));
+    }
+
+    private static Stream<Arguments> createAllGoals() {
+        return Stream.of(
+                Arguments.arguments(5, "Italy"),
+                Arguments.arguments(2, "Wales"),
+                Arguments.arguments(1, "Poland")
+        );
+    }
+
     @Test
     void mostGoalsCountry() {
+        assertEquals("Belgium", gameService.mostGoalsCountry());
     }
 }
