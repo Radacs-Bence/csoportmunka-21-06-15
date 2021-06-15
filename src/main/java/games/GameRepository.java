eed4817ee3a313b17384f00e927407f629a684f1
Radacs-Bence <bence.radacs@gmail.com>
2021-06-15 15:54:02 +0200

    Constuctors and getters

--- src/main/java/games/GameRepository.java
+++ src/main/java/games/GameRepository.java
@@ -1,8 +1,13 @@
 package games;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameRepository {
 
-    private List<Game> games;
+    private List<Game> games = new ArrayList<>();
+
+    public List<Game> getGames() {
+        return new ArrayList<>(games);
+    }
 }
