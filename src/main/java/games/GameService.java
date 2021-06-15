36bcfaae71977305914789b5d5cd3f12b20450e9
Radacs-Bence <bence.radacs@gmail.com>
2021-06-15 16:11:58 +0200

    largestGoalDiff()

--- src/main/java/games/GameService.java
+++ src/main/java/games/GameService.java
@@ -1,10 +1,17 @@
 package games;
 
+import java.util.Comparator;
+
 public class GameService {
 
     private GameRepository gameRepository;
 
     public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
+    }
+
+    public Game largestGoalDiff(){
+        return gameRepository.getGames().stream()
+                .max(Comparator.comparing(game -> Math.abs(game.getFirstCountryScore() - game.getSecondCountryScore()))).get();
     }
 }
