541b7fbd152efd33fa9a890b74efc12fddcc88aa
Radacs-Bence <bence.radacs@gmail.com>
2021-06-15 15:46:45 +0200

    Initial classes

---
+++ src/main/java/games/Game.java
@@ -0,0 +1,10 @@
+package games;
+
+public class Game {
+
+    private String firstCountry;
+    private String secondCountry;
+    private int firstCountryScore;
+    private int secondCountryScore;
+
+}
---
+++ src/main/java/games/GameRepository.java
@@ -0,0 +1,8 @@
+package games;
+
+import java.util.List;
+
+public class GameRepository {
+
+    private List<Game> games;
+}
