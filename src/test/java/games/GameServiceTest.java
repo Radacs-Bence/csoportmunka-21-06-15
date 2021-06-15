95897be80aae80318b03e25bca19d17ef4c6b6f0
Radacs-Bence <bence.radacs@gmail.com>
2021-06-15 17:33:26 +0200

    GameServiceTest init()

--- src/test/java/games/GameServiceTest.java
+++ src/test/java/games/GameServiceTest.java
@@ -1,10 +1,20 @@
 package games;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
+
+    GameService gameService;
+
+    @BeforeEach
+    void init() {
+        GameRepository gameRepository = new GameRepository();
+        gameRepository.addGameFromCsv("src/main/resources/results.csv");
+        gameService = new GameService(gameRepository);
+    }
 
     @Test
     void largestGoalDiff() {
