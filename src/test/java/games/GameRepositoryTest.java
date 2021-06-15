2eb2fd367af057cbb4941c6afa0c2980523631b3
BirkasTivadar <birkastivadar@gmail.com>
2021-06-15 16:43:33 +0200

    2-es test készül

--- src/test/java/games/GameRepositoryTest.java
+++ src/test/java/games/GameRepositoryTest.java
@@ -1,13 +1,27 @@
 package games;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameRepositoryTest {
 
+    GameRepository gameRepository;
+
+    @BeforeEach
+    void init(){
+        gameRepository = new GameRepository();
+    }
+
     @Test
     void addGame() {
+        Game game1 = new Game("Hungary", "Portugal", 4, 1);
+        Game game2 = new Game("Hungary", "France", 2, 2);
+
+        gameRepository.addGame(game1);
+        gameRepository.addGame(game2);
+
     }
 
     @Test
