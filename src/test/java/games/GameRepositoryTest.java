36d094180a94a2b8222a80211717cc1e8743db4b
Radacs-Bence <bence.radacs@gmail.com>
2021-06-15 16:34:33 +0200

    tests

---
+++ src/test/java/games/GameRepositoryTest.java
@@ -0,0 +1,16 @@
+package games;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameRepositoryTest {
+
+    @Test
+    void addGame() {
+    }
+
+    @Test
+    void addGameFromCsv() {
+    }
+}
