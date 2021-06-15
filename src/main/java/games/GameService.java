48afae5d5791964a5069d20ebabeb3a61330a43a
BirkasTivadar <birkastivadar@gmail.com>
2021-06-15 16:27:19 +0200

    5-öss kész elvileg

--- src/main/java/games/GameService.java
+++ src/main/java/games/GameService.java
@@ -10,8 +10,23 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
-    public Game largestGoalDiff(){
+    public Game largestGoalDiff() {
         return gameRepository.getGames().stream()
                 .max(Comparator.comparing(game -> Math.abs(game.getFirstCountryScore() - game.getSecondCountryScore()))).get();
+    }
+
+    public int allGoalsOfCountry(String country) {
+        int result = 0;
+
+        for (Game game : gameRepository.getGames()) {
+            if (game.getFirstCountry().equals(country)) {
+                result += game.getFirstCountryScore();
+            }
+            if (game.getSecondCountry().equals(country)) {
+                result += game.getSecondCountryScore();
+            }
+        }
+
+        return result;
     }
 }
